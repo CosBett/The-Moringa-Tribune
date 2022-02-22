e385bdb50f480533444cc040396f02ba73212141
CosBett <cosmasbett9@gmail.com>
2022-02-22 15:47:05 +0300

    add models to admin Page

--- news/tests.py
+++ news/tests.py
@@ -15,3 +15,16 @@ class EditorTestClass(TestCase):
     self.mandela.save_editor()
     editors = Editor.objects.all()
     self.assertTrue(len(editors) > 0)
+
+# class ArticleTestClass(TestCase):
+#   # Set up method
+#   def setUp(self):
+#     self.mandela = Editor(first_name = 'mandela', last_name ='Bett', email = 'cosmasbett9@gmail.com')
+#   # test instance
+#   def test_instance(self):
+#     self.assertTrue(isinstance(self.mandela,Editor))   
+#   # Testing Save method
+#   def test_save_method(self):
+#     self.mandela.save_editor()
+#     editors = Editor.objects.all()
+#     self.assertTrue(len(editors) > 0)
