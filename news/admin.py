c865ab06f0333a4fb0e631f9eff2b1948b2ccade
CosBett <cosmasbett9@gmail.com>
2022-02-23 11:27:32 +0300

    create and add new articles through the admin

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import tags,Editor, Article
-
+ 
+class ArticleAdmin(admin.ModelAdmin):
+  filter_horizontal = ('tags',) 
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(tags)
 
