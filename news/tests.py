c995c0e2a7a5bb96d41fa02071e93a2224f59504
CosBett <cosmasbett9@gmail.com>
2022-02-23 11:38:22 +0300

    create tests for article class

--- news/tests.py
+++ news/tests.py
@@ -16,15 +16,22 @@ class EditorTestClass(TestCase):
     editors = Editor.objects.all()
     self.assertTrue(len(editors) > 0)
 
-# class ArticleTestClass(TestCase):
-#   # Set up method
-#   def setUp(self):
-#     self.mandela = Editor(first_name = 'mandela', last_name ='Bett', email = 'cosmasbett9@gmail.com')
-#   # test instance
-#   def test_instance(self):
-#     self.assertTrue(isinstance(self.mandela,Editor))   
-#   # Testing Save method
-#   def test_save_method(self):
-#     self.mandela.save_editor()
-#     editors = Editor.objects.all()
-#     self.assertTrue(len(editors) > 0)
+class ArticleTestClass(TestCase):
+  # Set up method
+  def setUp(self):
+    self.mandela = Editor(first_name = 'mandela', last_name ='Bett', email = 'cosmasbett9@gmail.com')
+    self.mandela.save_editor()
+
+  # creating a new tag and saving it
+    self.new_tag =tags(name ='testing')
+    self.new_tag.save()
+
+    self.new_article = Article(title ='Test Article', post ='This is a random test article', editor ='self.mandela')
+    self.new_article.save()
+    self.new_article.tags.add(self.new_tag)
+
+  def tearDown(self):
+    Editor.objects.all().delete()
+    tags.objects.all().delete()
+    Article.objects.all().delete() 
+  
